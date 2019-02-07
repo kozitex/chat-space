0d00e619e2e406dbfe2ff357494f2ca861f0af31
kozitex <grassrunners@gmail.com>
2019-02-07 12:54:26 +0900

    Create model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
+  resources :groups, except: [:index, :show, :destroy]
   root  'messages#index'
 end
