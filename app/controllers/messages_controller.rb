6a2c098efd17df746bbe59b4750efb7ae82ed406
kozitex <grassrunners@gmail.com>
2019-02-07 20:51:26 +0900

    Mounting the function of creating groups

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,6 @@
 class MessagesController < ApplicationController
+  # def index
+  #   @groups = Group.includes(:group_users).where(group_users: { user_id: current_user.id })
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
-  resources :groups, except: [:index, :show, :destroy]
+  resources :groups, except: [:index, :show, :destroy] do
+    resources :messages, only: [:index, :create]
+  end
   root  'messages#index'
 end
