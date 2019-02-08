e9441b116ffb23d5802a7396060498146bb94437
kozitex <grassrunners@gmail.com>
2019-02-08 17:15:37 +0900

    Install carrierwave

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,19 @@
 class MessagesController < ApplicationController
   def index
-    if params[:group_id]
-      @current_group = Group.find(params[:group_id])
+    @current_group = Group.find(params[:group_id])
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if !@message.save
+      render :index, notice: 'メッセージを入力してください。'
     end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body, :image, :image_cache, :group_id, :user_id)
   end
 end
