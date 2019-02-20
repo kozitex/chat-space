f0820039c079f7db06aeccdd2a7b855694a9c389
kozitex <grassrunners@gmail.com>
2019-02-20 21:10:20 +0900

    Create another function

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -8,7 +8,16 @@ $(function() {
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
                 </div>`;
     result_list.append(html);
-  }
+  };
+
+  function buildHTML(user_id, user_name) {
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
+                  <p class='chat-group-user__name'>${ user_name }</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`;
+    return html;
+  };
 
   $('.chat-group-form__input').on('keyup', function() {
     var input = $(this).val();
@@ -39,11 +48,7 @@ $(function() {
     var user_id = $(this).attr('data-user-id');
     var user_name = $(this).attr('data-user-name');
     $(this).parent().remove();
-    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-                  <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
-                  <p class='chat-group-user__name'>${ user_name }</p>
-                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
-                </div>`;
+    var html = buildHTML(user_id, user_name);
     $('#chat-group-users').append(html);
   });
 
