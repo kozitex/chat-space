0cd8d3c60512a16b778c316c7e6b243416a357da
kozitex <grassrunners@gmail.com>
2019-02-20 20:16:33 +0900

    Mounting the function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
   resources :groups, except: [:index, :show, :destroy] do
     resources :messages, only: [:index, :create]
   end
